09020b6b817b78defdad6bdce47038e87813be37
Gustavo A. Inocencio <gusthavoinocencio@gmail.com>
2026-04-06 12:35:39 -0300

    Delete backend directory

--- backend/src/health.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class HealthController {
-  @Get()
-  healthCheck() {
-    return { status: 'OK', message: 'Backend rodando!', datetime: new Date() };
-  }
-}
